1dde797882f3c52512dafcda92342d7717f296f9
Omanshu840 <56030298+Omanshu840@users.noreply.github.com>
2020-03-20 18:26:37 +0530

    updated main.py

--- main.py
+++ main.py
@@ -31,7 +31,57 @@ def page2():
     plt.title('Green Area Graph') 
     plt.savefig('static/img/green.jpg')
 
-    return render_template('page2.html', per_g=per_g)
+    num_cycle = 0
+
+    date_sowing = [0]*24
+    ds = -1
+
+    date_harvest = [0]*24
+    dh = -1
+
+    for i in range(0,24):
+        if(per_g[i]>5):
+            num_cycle = num_cycle+1
+        
+            l = i - 1
+            while(1):
+                if(per_g[l]<1):
+                    ds+=1
+                    y = 2017
+                    if(l>13):
+                        l = l-12
+                        y +=1
+                    
+                    l = l-1
+                    s = "15 - "+str(l)+" - "+str(y)+""
+                    date_sowing[ds] = s
+                    break;
+                l+=-1
+                if(l<0):
+                    break;
+                    
+            l = i+1
+            while(1):
+                if(per_g[l]<1):
+                    dh+=1
+                    y = 2017
+                    if(l>13):
+                        l = l-12
+                        y +=1
+                    l = l-1
+                    s = "15 - "+str(l)+" - "+str(y)+""
+                    date_harvest[dh] = s;
+                    break;
+                l+=1
+                if(l>24):
+                    break;
+
+
+                            
+
+
+
+    return render_template('page2.html', per_g=per_g, num_cycle=num_cycle, date_sowing=date_sowing, ds=ds, date_harvest=date_harvest, dh=dh)
 
 
 @app.route("/test")
